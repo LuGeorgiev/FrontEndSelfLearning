4278af4dfe6bdae569bce16125926fcc27e7ebb7
Lyubomir Georgiev <LuGeorgiev@globul.bg>
2018-11-25 21:41:52 +0200

    Calendar

--- JS/SoftUni.Js.Advanced/jQueryExcercises/scripts/dom-search.js
+++ JS/SoftUni.Js.Advanced/jQueryExcercises/scripts/dom-search.js
@@ -20,15 +20,40 @@ function domSearch(selector, isCaseSensitive) {
     let listDiv = $('<div>').addClass('result-controls');
     let list = $('<ul class=items-list></ul>');
     list.appendTo(listDiv);
-
     addDiv.appendTo(fragment);
     searchDiv.appendTo(fragment);
     listDiv.appendTo(fragment);
     container.append(fragment);
 
+    $('.search-controls > label > input').keyup(function() {
+        let searchText = $(this).val();
+        let listElements = $('.list-item strong').toArray();
+        for (let element of listElements) {
+            let current = $(element);
+
+            if (isCaseSensitive) {
+                if (current.text().indexOf(searchText) < 0) {
+                    current.parent().css('display', 'none');
+                } else {
+                    current.parent().css('display', '');
+                }
+            } else {
+                if (current.text().toLowerCase().indexOf(searchText.toLowerCase()) < 0) {
+                    current.parent().css('display', 'none');
+                } else {
+                    current.parent().css('display', '');
+                }
+            }
+        }
+    });
+
     $('.add-controls > a ').on('click', function() {
         let text = $('.add-controls > label > input').val();
+        list.append(`<li class=list-item ><a class=button>X</a><strong>${text}</strong></li>`);
 
-        list.append(`<li><a class=button>X</a><strong>${text}</strong></li>`);
+        $('.list-item > a').on('click', function() {
+            $(this).parent().remove();
+        });
     });
+
 }
