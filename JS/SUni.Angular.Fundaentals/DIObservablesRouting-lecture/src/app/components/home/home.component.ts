5215c49bb4dd87978879a91ec529590838da96e1
LuGeorgiev <lubomir_78@abv.bg>
2019-03-19 22:57:41 +0200

    DI, Http and Observables Lecture - Kenov

---
+++ JS/SUni.Angular.Fundaentals/DIObservablesRouting-lecture/src/app/components/home/home.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { HomeService } from './home.service';
+import { GitHubProfile } from './github-profile';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+  providers: [HomeService]
+})
+export class HomeComponent implements OnInit {
+  someData: string;
+  profile:GitHubProfile;
+
+  constructor(
+    private homeService: HomeService) {
+    this.someData = homeService.getDate();
+    
+  }
+
+  ngOnInit() {
+    console.log(this.someData);
+    this.homeService
+      .gteGitHubProfile('lugeorgiev')
+      .subscribe(data=> this.profile=data);
+  }
+}
---
+++ JS/SUni.Angular.Fundaentals/DIObservablesRouting-lecture/src/app/components/home/home.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from '@angular/common/http';
+import { GitHubProfile } from './github-profile';
+
+@Injectable()
+export class HomeService {
+    constructor(private httpClient:HttpClient){
+    }
+    
+    getDate(){
+        return "Hello from service";
+    }
+
+    gteGitHubProfile(profile:string){
+        const url = `https://api.github.com/users/${profile}`
+
+        return this.httpClient
+             .get<GitHubProfile>(url);
+    }
+}
