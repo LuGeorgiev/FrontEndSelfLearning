603ba0f01db3d9ca5b8c2cea9f485dc57ffe76f2
LuGeorgiev <lubomir_78@abv.bg>
2019-08-02 22:31:21 +0300

    Books controller

---
+++ JS/SUni.Angular.Fundaentals/auth-c-sharp/ApiAngularAuth/Controllers/BookController.cs
@@ -0,0 +1,33 @@
+﻿using ApiAngularAuth.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiAngularAuth.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private readonly BookStoreContext db; 
+
+        public BookController(BookStoreContext db)
+        {
+            this.db = db;
+        }
+
+        [HttpGet]
+        private async Task<IActionResult> GetBooks()
+        {
+            var data = await db.Books.ToListAsync();
+
+            return this.Ok(data);
+        }
+
+    }
+}
