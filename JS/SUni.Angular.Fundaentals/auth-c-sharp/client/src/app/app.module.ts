800750c1daed7eb9d3a39cc1f82d77bf05efe2f8
LuGeorgiev <lubomir_78@abv.bg>
2019-08-03 16:58:01 +0300

    Angular client created with basic component

---
+++ JS/SUni.Angular.Fundaentals/auth-c-sharp/client/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BookComponent } from './book/book.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BookComponent,
+    LoginComponent,
+    RegisterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
