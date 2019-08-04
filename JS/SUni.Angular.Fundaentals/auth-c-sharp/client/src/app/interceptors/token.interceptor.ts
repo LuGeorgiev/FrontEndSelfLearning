a86a3749fded066028b2f35edc96892f773cd85c
LuGeorgiev <lubomir_78@abv.bg>
2019-08-04 10:35:26 +0300

    token interceptor

---
+++ JS/SUni.Angular.Fundaentals/auth-c-sharp/client/src/app/interceptors/token.interceptor.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@angular/core';
+import { 
+    HttpRequest,
+    HttpResponse,
+    HttpHandler,
+    HttpEvent,
+    HttpInterceptor,
+    HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+import { Router } from '@angular/router';
+
+@Injectable() export class TokenInterceptor implements HttpInterceptor{
+
+    constructor(private router:Router) { } 
+
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        const token = localStorage.get('token');
+
+        if (token) {
+           req = req.clone({
+               setHeaders:{
+                   'Authorization':'Bearer' + 'token'
+               }
+           });
+        }
+        if (!req.headers.has('Content-type')) {
+            req = req.clone({
+                setHeaders:{
+                    'content-type':'application/json'
+                }
+            });
+        }
+        req = req.clone({
+            headers: req.headers.set('Accept','application/json')
+        });
+
+        return next.handle(req).pipe(
+            map((event:HttpEvent<any>) => {
+                if (event instanceof HttpResponse ) {
+                    console.log('event-->', event);
+                }
+                return event;
+            }),
+            catchError((error:HttpErrorResponse) => {
+                console.log(error);
+                if (error.status === 400) {
+                    alert(error.error);
+                }
+                return throwError(error);
+            })
+        );
+    }
+
+}
