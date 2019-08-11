d1138f5a8dfa8fcb4a8874e391d0a8b0f7916416
LuGeorgiev <lubomir_78@abv.bg>
2019-08-11 14:01:01 +0300

    reactive registration form implemented

--- JS/SUni.Angular.Fundaentals/registration-form/src/app/register-reactive/register-reactive.component.ts
+++ JS/SUni.Angular.Fundaentals/registration-form/src/app/register-reactive/register-reactive.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { FormBuilder, Validators, FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-register-reactive',
@@ -8,13 +8,24 @@ import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 })
 export class RegisterReactiveComponent implements OnInit {
   form: FormGroup;
+  phonePrefix: string[] = ["+359", "+789", "+100", "+897", "+345"];
+  jobTitles: string[] = ["Designer", "Manager", "Junior Developer", "Senior Developer", "Application Architect", "Other"];
+
 
   constructor(private fb:FormBuilder, ) { }
 
   ngOnInit() {
     this.form = this.fb.group({
-      fullName: ['', [Validators.required, Validators.pattern(/^[a-zA-Z0-9_-]+$/)],[]],
-      email : ['', [Validators.required, Validators.email],[]]
+      fullName: ['', [Validators.required, Validators.pattern(/[A-Z][a-z]+\s[A-Z][a-z]+/)],[]],
+      email: ['', [Validators.required, Validators.email],[]],
+      image: ['',[Validators.required, Validators.pattern(/^(http:\/\/|https:\/\/)[a-zA-z\.0-9\/]+(.png|.jpg)$/)], [] ],
+      prefix:['',[Validators.required],[]],
+      phone:['', [Validators.pattern(/^[\d]{9}$/)],[]],
+      jobTitle: ['', [Validators.required],[]],
+      passwords: this.fb.group({
+        password: ['',[Validators.required, Validators.pattern(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{3,16}$/)],[]],
+        confirmedPasword: ['', [Validators.required],[]]
+      },{validators: this.validatePasswordEquality})      
     })
   }
 
@@ -22,7 +33,19 @@ export class RegisterReactiveComponent implements OnInit {
     console.log(this.form);
   }
 
-  get f(){
+  validatePasswordEquality (group: FormGroup){
+    const pass = group.controls['password'];
+    const confirmPass = group.controls['confirmedPasword'];
+
+    if (pass.value !== confirmPass.value) {
+      confirmPass.setErrors({validatePasswordEquality:true});
+    } else {
+      confirmPass.setErrors(null);
+    }
+  }
+
+  get f (){
+
     return this.form.controls;
   }
 }
