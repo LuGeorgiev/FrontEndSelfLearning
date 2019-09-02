f807dbacf89dc731d83cdb1c611752a41896747a
LuGeorgiev <lubomir_78@abv.bg>
2019-09-02 22:58:46 +0300

    toastr ready

--- JS/SUni.Angular.Fundaentals/furniture-system/client-furniture-system/src/app/app.module.ts
+++ JS/SUni.Angular.Fundaentals/furniture-system/client-furniture-system/src/app/app.module.ts
@@ -22,6 +22,7 @@ import { FurnitureCreateComponent } from './furniture/furniture-create/furniture
 import { FurnitureDetailsComponent } from './furniture/furniture-details/furniture-details.component';
 import { FurnitureUserComponent } from './furniture/furniture-user/furniture-user.component';
 import { FurnitureService } from './furniture/furniture.service';
+import { ResponseHandlerInterceptorService } from './response-handler-interceptor.service';
 
 @NgModule({
   declarations: [
@@ -49,7 +50,8 @@ import { FurnitureService } from './furniture/furniture.service';
   providers: [ 
     AuthService,
     FurnitureService,
-    { provide: HTTP_INTERCEPTORS , useClass: JwtInterceptorService, multi:true }
+    { provide: HTTP_INTERCEPTORS , useClass: JwtInterceptorService, multi:true },
+    { provide: HTTP_INTERCEPTORS , useClass: ResponseHandlerInterceptorService, multi:true },
   ],
   bootstrap: [AppComponent]
 })
