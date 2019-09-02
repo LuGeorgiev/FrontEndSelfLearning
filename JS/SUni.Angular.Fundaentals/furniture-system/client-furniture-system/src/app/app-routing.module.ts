ff06a55b4d14197de78fa5ae0f3765c9bf1da787
LuGeorgiev <lubomir_78@abv.bg>
2019-09-02 23:19:51 +0300

    lazy loading included for furniture

--- JS/SUni.Angular.Fundaentals/furniture-system/client-furniture-system/src/app/app-routing.module.ts
+++ JS/SUni.Angular.Fundaentals/furniture-system/client-furniture-system/src/app/app-routing.module.ts
@@ -5,21 +5,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { SigninComponent } from './authentication/signin/signin.component';
 import { SignupComponent } from './authentication/signup/signup.component';
-import { FurnitureCreateComponent } from './furniture/furniture-create/furniture-create.component';
 import { AuthGuard } from './authentication/guards/auth.guard';
-import { FurnitureAllComponent } from './furniture/furniture-all/furniture-all.component';
-import { FurnitureDetailsComponent } from './furniture/furniture-details/furniture-details.component';
-import { FurnitureUserComponent } from './furniture/furniture-user/furniture-user.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: 'home', component: HomeComponent },
   { path: 'signin', component: SigninComponent },
   { path: 'signup', component: SignupComponent },
-  { path: 'furniture/create', component: FurnitureCreateComponent, canActivate:[AuthGuard]},
-  { path: 'furniture/all', component: FurnitureAllComponent, canActivate:[AuthGuard]},
-  { path: 'furniture/details/:id', component: FurnitureDetailsComponent , canActivate:[AuthGuard]},
-  { path: 'furniture/user', component: FurnitureUserComponent , canActivate:[AuthGuard]},
+  { path : 'furniture', loadChildren:'./furniture/furniture.module#FurnitureModule', canActivate:[AuthGuard]}
 ]
 
 @NgModule({
--- JS/SUni.Angular.Fundaentals/furniture-system/client-furniture-system/src/app/app.module.ts
+++ JS/SUni.Angular.Fundaentals/furniture-system/client-furniture-system/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -17,11 +17,6 @@ import { ToastrModule } from 'ngx-toastr';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { JwtInterceptorService } from './jwt-interceptor.service';
 
-import { FurnitureAllComponent } from './furniture/furniture-all/furniture-all.component';
-import { FurnitureCreateComponent } from './furniture/furniture-create/furniture-create.component';
-import { FurnitureDetailsComponent } from './furniture/furniture-details/furniture-details.component';
-import { FurnitureUserComponent } from './furniture/furniture-user/furniture-user.component';
-import { FurnitureService } from './furniture/furniture.service';
 import { ResponseHandlerInterceptorService } from './response-handler-interceptor.service';
 
 @NgModule({
@@ -33,23 +28,17 @@ import { ResponseHandlerInterceptorService } from './response-handler-intercepto
     HomeComponent,
     DropdownDirective,
     CollapseDirective,
-    FurnitureAllComponent,
-    FurnitureCreateComponent,
-    FurnitureDetailsComponent,
-    FurnitureUserComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    FormsModule,
-    ReactiveFormsModule,
+    FormsModule,    
     AppRoutingModule,
     HttpClientModule,
     ToastrModule.forRoot(),
   ],
   providers: [ 
     AuthService,
-    FurnitureService,
     { provide: HTTP_INTERCEPTORS , useClass: JwtInterceptorService, multi:true },
     { provide: HTTP_INTERCEPTORS , useClass: ResponseHandlerInterceptorService, multi:true },
   ],
