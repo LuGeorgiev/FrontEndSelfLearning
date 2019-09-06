29a56d369cf9bbbb244772ed9f7faeb6bb921188
LuGeorgiev <lubomir_78@abv.bg>
2019-09-06 15:30:40 +0300

    test 3

--- JS/SUni.Angular.Fundaentals/seen-it/web-client/src/app/kinvey.tokens.ts
+++ JS/SUni.Angular.Fundaentals/seen-it/web-client/src/app/kinvey.tokens.ts
@@ -1,2 +1,2 @@
-export const APP_KEY = 'test2' // Kinvey APP KEY here;
-export const APP_SECRET = 'test2' // Kinvey APP SECRET here;
+export const APP_KEY = 'test3' // Kinvey APP KEY here;
+export const APP_SECRET = 'test3' // Kinvey APP SECRET here;
