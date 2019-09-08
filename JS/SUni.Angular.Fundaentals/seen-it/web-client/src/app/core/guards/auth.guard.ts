5c6352478ffde1bced06613534158be9339a06f2
LuGeorgiev <lubomir_78@abv.bg>
2019-09-08 14:52:08 +0300

    auth guard for lazy loadde module

---
+++ JS/SUni.Angular.Fundaentals/seen-it/web-client/src/app/core/guards/auth.guard.ts
@@ -0,0 +1,32 @@
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router, CanLoad, Route, UrlSegment } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class AuthGuard implements CanLoad {
+    constructor(private authService:AuthService,
+                private router: Router){}
+
+    canLoad(route: Route, segments: UrlSegment[]) {
+        if(this.authService.isAuthenticated()){
+            return true
+        }
+
+        this.router.navigate(['/login']);
+        return false;
+    }
+
+
+
+    // canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot){
+    //     if(this.authService.isAuthenticated()){
+    //         return true
+    //     }
+
+    //     this.router.navigate(['/login']);
+    //     return false;
+    // }
+
+}
