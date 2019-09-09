2c59258b3899031edff2149de377227a5daefac5
LuGeorgiev <lubomir_78@abv.bg>
2019-09-09 22:30:45 +0300

    side navigation unedr construction

--- JS/SUni.Angular.Fundaentals/sli-do-materia/src/app/app.module.ts
+++ JS/SUni.Angular.Fundaentals/sli-do-materia/src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,7 +15,8 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- JS/SUni.Angular.Fundaentals/sli-do-materia/src/app/material.module.ts
+++ JS/SUni.Angular.Fundaentals/sli-do-materia/src/app/material.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule, MatListModule } from '@angular/material';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 @NgModule({
     imports:[
-        MatButtonModule
+        MatButtonModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatSidenavModule,
+        MatListModule
     ],
     exports:[
-        MatButtonModule
+        MatButtonModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatSidenavModule,
+        MatListModule
     ]
 })
 export class MaterialModule{
