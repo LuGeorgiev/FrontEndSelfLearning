5f5467844c5df218320cc5fba5ba51470b1ee1cf
LuGeorgiev <lubomir_78@abv.bg>
2019-09-15 18:14:22 +0300

    building question componnets

--- JS/SUni.Angular.Fundaentals/sli-do-materia/src/app/app-routing.module.ts
+++ JS/SUni.Angular.Fundaentals/sli-do-materia/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { FullscreenOverlayContainer } from '@angular/cdk/overlay';
 import { HomeComponent } from './components/home/home.component';
+import { EventLiveComponent } from './components/events/event-live/event-live.component';
 
 const routes: Routes = [
-  {path:'', pathMatch:'full', component: HomeComponent},
-  {path:'home', component: HomeComponent},
+  { path:'', pathMatch:'full', component: HomeComponent },
+  { path:'home', component: HomeComponent },
+  { path: 'event/:id/live', component: EventLiveComponent }
 ];
 
 @NgModule({
---
+++ JS/SUni.Angular.Fundaentals/sli-do-materia/src/app/components/events/event-post-question/event-post-question.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-event-post-question',
+  templateUrl: './event-post-question.component.html',
+  styleUrls: ['./event-post-question.component.css']
+})
+export class EventPostQuestionComponent implements OnInit {
+
+  postQuestionForm: FormGroup
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit() {
+    this.postQuestionForm = this.fb.group({
+      text: [null,[Validators.required, Validators.minLength(6), Validators.maxLength(200)],[]]
+    });
+  }
+
+}
